c42fd2df347ae2326460bcb2f85bd3af2cb1803a
CtrlAustin <89940899+CtrlAustin@users.noreply.github.com>
2024-07-25 22:09:37 -0600

    Update magilist.js
    Added integrated tooltips and support for multiple lists

--- magilist.js
+++ magilist.js
@@ -6,33 +6,63 @@
 // ###########################################################
 
 // user elements:
-const listWrapperName = 'musicwrapper';              // The class name of a wrapper for all that magilist does
-const listFilePath = 'list.json';                    // The path to yout JSON file including the file name
+const showTooltips = true;                           // Show ro hide tooltips
+const tooltipClassName = 'tooltip';                  // The class that the tooltip has
+const tooltipOffsetX = 10;                           // Offsets for the tooltip
+const tooltipOffsetY = 10;
 
 // advanced elements:
+const listFilePath = 'list.json';                    // The path to yout JSON file including the file name
 const tooltipTag = 'p';                              // The tag that the tooltip uses
-const itemWrapperClass = 'itemWrapper'               // The class name of each item
 const itemTag = 'div';                               // The tag that every item has in the list
+const itemWrapperClass = 'itemWrapper'               // The class name of each item
 const customDataImg = '';                            // If you want to add custom data to the tooltip like an id
 const customDataTooltip = '';                        // If you want to add custom data to the tooltip 
-const tooltipClassName = 'tooltip';                  // The class that the tooltip has
 const errorMessage = 'Error fetching JSON data:';    // If you want a custom error message
 
 
 
-// ``**########## !main code - avoid messing with this! ############**``
-const parentContainer = document.getElementsByClassName(listWrapperName)[0];
+// ``**########## ! main code - avoid messing with this ! ############**``
 const filePath = listFilePath;
 
 fetch(filePath)
     .then(response => response.json())
     .then(data => {
-        const list = data.list;
-        list.forEach(item => {
-            const div = document.createElement(itemTag);
-            div.classList.add(itemWrapperClass)
-            div.innerHTML = `<img ${customDataImg} src="${item.image}" alt="${item.alt}"> <${tooltipTag} ${customDataTooltip} class="${tooltipClassName}"> ${item.tooltip} </${tooltipTag}>`;
-            parentContainer.appendChild(div);
+        const lists = data.lists;
+        lists.forEach(list => {
+            if (document.getElementById(list.name)) {
+                var parentContainer = document.getElementById(list.name);
+                list.items.forEach(item => {
+                    const div = document.createElement(itemTag);
+                    div.classList.add(itemWrapperClass);
+                    const img = document.createElement('img');
+                    img.src = item.image;
+                    img.alt = item.alt;
+                    if (showTooltips) {
+                        img.addEventListener('mousemove', (event) => {
+                            tooltip.style.left = event.clientX + tooltipOffsetX + 'px';
+                            tooltip.style.top = event.clientY + tooltipOffsetY + 'px';
+                            tooltip.style.zIndex = '999';
+                            tooltip.style.display = 'block';
+                            tooltip.style.position = 'fixed';
+                        });
+                        img.addEventListener('mouseout', function () {
+                            tooltip.style.display = 'none';
+                        });
+                    }
+                    div.appendChild(img);
+                    const tooltip = document.createElement(tooltipTag);
+                    tooltip.classList.add(tooltipClassName);
+                    tooltip.textContent = item.tooltip;
+                    tooltip.style.display = 'none';
+                    div.appendChild(tooltip);
+                    parentContainer.appendChild(div);
+                });
+                console.log("found element")
+            } else {
+                console.log("didnt find " + list.name)
+            }
+
         });
     })
     .catch(error => {
